0dca6cd574bd85f272bd88e41ce90db5c5b2212d
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-04-18 19:13:09 -0700

    testing

--- scripts/bot.js
+++ scripts/bot.js
@@ -10,65 +10,66 @@ const name = (string) => {
 
 const bot = async (user, request) => {
   // If request has length, it's a string, handle logic
-  if (request.length) {
-    // Recognize activity
-    const activityId = await axios
-      .get(
-        `https://remindapp-server.herokuapp.com/api/activity/${name(request)}`
-      )
-      .then((data) => {
-        return data.data[0].id;
-      })
-      .catch((e) => e);
-    // Find who the user is
-    const userId = await axios
-      .get(`https://remindapp-server.herokuapp.com/api/phone/${user}`)
-      .then((data) => {
-        return data.data[0].id;
-      })
-      .catch((e) => e);
-    // Check if they have a habit
-    const habitId = await axios
-      .get(`https://remindapp-server.herokuapp.com/api/user/${userId}/habits`)
-      .then((data) => {
-        let result = data.data;
-        let habit = null;
-        result.forEach((element) => {
-          if (element.activity_id === activityId) {
-            habit = element.id;
-          }
-        });
-        return habit;
-      })
-      .catch((e) => e);
-    if (habitId !== null) {
-      console.log("habit recorded");
-      recordActivity(habitId, (err, items) => {
-        if (err) {
-          console.log("Error");
-          res.sendStatus(404);
-        } else {
-          twiml.message("Thanks! Keep up the good work!");
-          res.writeHead(200, { "Content-Type": "text/xml" });
-          res.end(twiml.toString());
-        }
-      });
-    } else {
-      console.log("habit not recognized");
-    }
-  } else {
-    console.log("habit_id received");
-    recordActivity(habitId, (err, items) => {
-      if (err) {
-        console.log("Error");
-        res.sendStatus(404);
-      } else {
-        twiml.message("Thanks! Keep up the good work!");
-        res.writeHead(200, { "Content-Type": "text/xml" });
-        res.end(twiml.toString());
-      }
-    });
-  }
+  console.log(user, request);
+  // if (request.length) {
+  //   // Recognize activity
+  //   const activityId = await axios
+  //     .get(
+  //       `https://remindapp-server.herokuapp.com/api/activity/${name(request)}`
+  //     )
+  //     .then((data) => {
+  //       return data.data[0].id;
+  //     })
+  //     .catch((e) => e);
+  //   // Find who the user is
+  //   const userId = await axios
+  //     .get(`https://remindapp-server.herokuapp.com/api/phone/${user}`)
+  //     .then((data) => {
+  //       return data.data[0].id;
+  //     })
+  //     .catch((e) => e);
+  //   // Check if they have a habit
+  //   const habitId = await axios
+  //     .get(`https://remindapp-server.herokuapp.com/api/user/${userId}/habits`)
+  //     .then((data) => {
+  //       let result = data.data;
+  //       let habit = null;
+  //       result.forEach((element) => {
+  //         if (element.activity_id === activityId) {
+  //           habit = element.id;
+  //         }
+  //       });
+  //       return habit;
+  //     })
+  //     .catch((e) => e);
+  //   if (habitId !== null) {
+  //     console.log("habit recorded");
+  //     recordActivity(habitId, (err, items) => {
+  //       if (err) {
+  //         console.log("Error");
+  //         res.sendStatus(404);
+  //       } else {
+  //         twiml.message("Thanks! Keep up the good work!");
+  //         res.writeHead(200, { "Content-Type": "text/xml" });
+  //         res.end(twiml.toString());
+  //       }
+  //     });
+  //   } else {
+  //     console.log("habit not recognized");
+  //   }
+  // } else {
+  //   console.log("habit_id received");
+  //   recordActivity(habitId, (err, items) => {
+  //     if (err) {
+  //       console.log("Error");
+  //       res.sendStatus(404);
+  //     } else {
+  //       twiml.message("Thanks! Keep up the good work!");
+  //       res.writeHead(200, { "Content-Type": "text/xml" });
+  //       res.end(twiml.toString());
+  //     }
+  //   });
+  // }
 };
 
 module.exports = { bot };
