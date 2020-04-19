b60469a0a29e80f648027c2cf55e31746be49f23
Artur Iatsko <arturiatsko@Arturs-MBP.hitronhub.home>
2020-04-18 17:06:50 -0700

    Bot bolerplate added

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,2 +1,29 @@
 const request1 = "track yoga";
 const request2 = "record running";
+const request3 = "please record run";
+const request4 = "pray";
+const request5 = 10;
+
+const name = (string) => {
+  const n = string.split(" ");
+  return n[n.length - 1];
+};
+
+const bot = (request) => {
+  if (request.length) {
+    console.log("text request", name(request));
+    // find who the customer is
+    // find the activity
+    // find habit and record habit
+    // congratulate customer
+  } else {
+    console.log("reply", request);
+    // implement recordActivity
+  }
+};
+
+bot(request1);
+bot(request2);
+bot(request3);
+bot(request4);
+bot(request5);
